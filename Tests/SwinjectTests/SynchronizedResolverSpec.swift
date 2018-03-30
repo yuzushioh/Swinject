5f846bef72f537c4e2705da659fbb8922a5c486c
yuzushioh <yuzushioh@gmail.com>
2018-03-30 13:18:27 +0900

    fix bugs

--- Tests/SwinjectTests/SynchronizedResolverSpec.swift
+++ Tests/SwinjectTests/SynchronizedResolverSpec.swift
@@ -46,7 +46,7 @@ class SynchronizedResolverSpec: QuickSpec {
                     let childResolver = Container(parent: parentContainer).synchronize()
 
                     onMultipleThreads(actions: [
-                        { _ = parentResolver.resolve(Animal.self) as! Cat },    // swiftlint:disable:this opening_brace
+                        { _ = parentResolver.resolve(Animal.self) as! Cat },
                         { _ = childResolver.resolve(Animal.self) as! Cat }
                     ])
                 }
